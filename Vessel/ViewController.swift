3c21fd1addda308f521f42d9b687bab8b985ab4f
SCrabtree <sean.crabtree@gmail.com>
2015-08-06 01:38:34 -0700

    Vessel 0.2

--- Vessel/ViewController.swift
+++ Vessel/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var timePicked: UIDatePicker!
     
     
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         
@@ -25,7 +26,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
